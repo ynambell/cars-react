aba0693219b042500ba509ec58045c363e523108
Yulia Scherbakova <ynambell@gmail.com>
2024-03-26 12:03:48 +0600

    lint fix

--- webpack.config.js
+++ webpack.config.js
@@ -5,36 +5,36 @@ module.exports = {
     mode: 'development',
     entry: './src/index.js',
     output: {
-    filename: 'bundle.js',
-},
-module: {
-    rules: [
-        {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-            loader: 'babel-loader',
-        },
-        },
-    {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        filename: 'bundle.js',
     },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+        }),
     ],
-},
-resolve: {
-    extensions: ['.js', '.jsx'],
-},
-plugins: [
-    new HtmlWebpackPlugin({
-        template: './public/index.html',
-    }),
-],
-devServer: {
-    static: {
-        directory: path.join(__dirname, 'dist'),
-    },
-    hot: true,
-    open: true,
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        hot: true,
+        open: true,
     },
 };
