6a85248d82f48f969160c30c9a280c2cfc68eb6a
Yulia Scherbakova <ynambell@gmail.com>
2024-03-27 10:22:51 +0600

    use other mockapi

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,19 @@
-import React, {useEffect, useState} from 'react';
+import {useEffect, useState} from 'react';
 import {CatalogContent} from '../CatalogContent/CatalogContent';
 import './App.css';
-import data from './data.json';
 
 export function App() {
     const [galleryItems, setGalleryItems] = useState([]);
 
-    useEffect(() => {
-        setGalleryItems(data);
-    }, [data]);
+    const apiUrl = 'https://demo5394722.mockable.io/cars';
 
-    // const apiUrl = 'https://demo5394722.mockable.io/cars';
-    // useEffect(() => {
-    //     fetch('')
-    //         .then((res) => (res.json()))
-    //         .then((data) => {
-    //             setGalleryItems(data);
-    //         });
-    // }, [apiUrl]);
+    useEffect(() => {
+        fetch('')
+            .then((res) => (res.json()))
+            .then((data) => {
+                setGalleryItems(data);
+            });
+    }, [apiUrl]);
 
     return (
         <CatalogContent galleryItems={galleryItems}/>
