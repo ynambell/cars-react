30c267d92582b175767766afb0ec6dec8c952ec0
Yulia Scherbakova <ynambell@gmail.com>
2024-03-28 11:15:49 +0600

    wrapp homepage

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,5 +1,10 @@
 import {Link} from 'react-router-dom';
+import {MainContent} from '../../components/MainContent/MainContent';
 
 export function HomePage() {
-    return <Link to="/catalog">Go to catalog</Link>;
+    return (
+        <MainContent>
+            <Link to="/catalog">Go to catalog</Link>;
+        </MainContent>
+    );
 }
