622a9e16a9d5790e308b3bc2185831588248853e
Yulia Scherbakova <ynambell@gmail.com>
2024-03-29 11:11:41 +0600

    if item is favorite on item page

--- src/pages/ItamPage.js
+++ src/pages/ItamPage.js
@@ -1,20 +1,25 @@
 import {useLoaderData} from 'react-router-dom';
+import {useSelector} from 'react-redux';
 import {MainContent} from '../components/MainContent/MainContent';
 import {Menu} from '../components/Menu/Menu';
 
 
 export function ItamPage() {
     const {
+        id,
         image,
         title,
         description,
     } = useLoaderData();
+
+    const isFavorite = useSelector((state) => (state.favorites.includes(id)));
+
     return (
         <>
             <Menu/>
             <MainContent>
                 <img src={image} alt={`${title} photo`}/>
-                <h1>{title}</h1>
+                <h1>{title} {isFavorite && <small>(favorite!)</small>}</h1>
                 <div>{description}</div>
             </MainContent>
         </>
